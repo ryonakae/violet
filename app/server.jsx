efac605077e6d3ac23466c67878a997f095225f2
Ryo Nakae <ryo.n.1129@gmail.com>
2015-10-28 02:19:38 +0900

    update

--- app/server.jsx
+++ app/server.jsx
@@ -1,20 +1,52 @@
 var express = require('express');
 var path = require('path');
-var app = express();
 var http = require('http');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var util = require('util');
+var TumblrStrategy = require('passport-tumblr').Strategy;
+var app = express();
+var MongoStore = require('connect-mongo')(session);
+
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes.jsx');
-
-// view engine
-app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 var env = require('./env');
 var TUMBLR_CONSUMER_KEY = env.TUMBLR_CONSUMER_KEY;
 var TUMBLR_SECRET_KEY = env.TUMBLR_SECRET_KEY;
 var TUMBLR_API_KEY = env.TUMBLR_API_KEY;
+
+// Passport session setup
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the TumblrStrategy within Passport
+passport.use(new TumblrStrategy({
+    consumerKey: TUMBLR_CONSUMER_KEY,
+    consumerSecret: TUMBLR_SECRET_KEY,
+    callbackURL: "http://localhost:3000/auth/tumblr/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // console.log(token, tokenSecret, profile);
+
+    // tokenなどを格納
+    profile.token = token;
+    profile.tokenSecret = tokenSecret;
+
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+));
 
 // data sample
 var superagent = require('superagent');
@@ -34,6 +66,32 @@ superagent
     data = res.body.response;
     // console.log(data);
   });
+
+// settings
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  store: new MongoStore({
+    db: 'session',
+    host: 'localhost',
+    clear_interval: 60 * 60 // 60 * 60 = 1 hour
+  }),
+  cookie: {
+    httpOnly: false,
+    maxAge: new Date(Date.now() + 60 * 60 * 1000) // 60 * 60 * 1000 = 3600000 msec = 1 hour
+  },
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routing
 app.use(function(req, res, next) {
