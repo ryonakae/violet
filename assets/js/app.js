9e3f585e3d6d4e14862ce19c52d3d74ce5528213
Ryo Nakae <ryo.n.1129@gmail.com>
2015-11-04 02:22:30 +0900

    jQueryとVelocity用にbrowserify-shim入れた

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
-var socketIO = require('./dependencies/socket.io');
-var io = require('./dependencies/sails.io')(socketIO);
-var $ = require('./dependencies/jquery.min');
+var socketIO = require('./dependencies/socket.io.js');
+var io = require('./dependencies/sails.io.js')(socketIO);
 
-(function(){
-  $('#button').on('click', function(){
-    io.socket.get('/dashboard/get', function serverRespondedWith (body, jwr){
-      console.log(body);
-    });
+require('jquery');
+require('velocity');
+
+$('#button').on('click', function(){
+  io.socket.get('/dashboard/get', function serverRespondedWith (body, jwr){
+    console.log(body);
   });
-})();
+
+  $(this).velocity({
+    opacity: 0
+  },{
+    duration: 500
+  });
+});
