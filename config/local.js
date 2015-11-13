4e398781b531d85cf5f9b9f76f43029ce5e9895f
Ryo Nakae <ryo.n.1129@gmail.com>
2015-11-13 10:38:27 +0900

    update

--- config/local.js
+++ config/local.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
-  // explicitHost: process.env.OPENSHIFT_APP_DNS || "localhost",
+  explicitHost: process.env.OPENSHIFT_APP_DNS || "localhost",
   port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
   environment: process.env.NODE_ENV || 'development'
 }
