74f6018d284cb17155f2e1fa321715fe35bf47a4
RYO NAKAE <ryo.n.1129@gmail.com>
2015-11-16 17:03:20 +0900

    update models

--- api/models/User.js
+++ api/models/User.js
@@ -6,8 +6,6 @@
 */
 
 module.exports = {
-  adapter: (process.env.NODE_ENV === 'production') ? 'productionPostgresqlServer' : 'somePostgresqlServer',
-
   attributes: {
     username: {
       type: 'text',
