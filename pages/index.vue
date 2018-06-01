c711f6135b37cf952313bac472b2f1a54c7a783c
Ryo Nakae <ryo.n.1129@gmail.com>
2018-06-01 17:49:17 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -4,3 +4,11 @@
     <nuxt-link to="/">sign in with Tumblr</nuxt-link>
   </div>
 </template>
+
+<script>
+export default {
+  mounted(){
+    console.log(this.$store.state.oauth)
+  }
+}
+</script>
