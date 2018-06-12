b2de91c3a706f3f17b2020148f9f6b3d8c6d6b76
Ryo Nakae <ryo.n.1129@gmail.com>
2018-06-12 16:20:35 +0900

    update

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -16,6 +16,9 @@
 </template>
 
 <script>
+import tumblr from 'tumblr.js'
+import config from '~/server/config'
+
 export default {
   computed: {
     isAuthed() {
@@ -24,6 +27,36 @@ export default {
 
     user() {
       return this.$store.state.user
+    }
+  },
+
+  methods: {
+    async getDashboard() {
+      // const data = await this.$axios.$get(
+      //   'https://api.tumblr.com/v2/user/dashboard',
+      //   {
+      //     params: {
+      //       consumer_key: config.TUMBLR_CONSUMER_KEY,
+      //       consumer_secret: config.TUMBLR_SECRET_KEY,
+      //       token: this.user.token,
+      //       token_secret: this.user.tokenSecret
+      //     }
+      //   }
+      // )
+
+      const client = tumblr.createClient({
+        credentials: {
+          consumer_key: config.TUMBLR_CONSUMER_KEY,
+          consumer_secret: config.TUMBLR_SECRET_KEY,
+          token: this.user.token,
+          token_secret: this.user.tokenSecret
+        },
+        returnPromises: true
+      })
+
+      const data = await client.userDashboard()
+
+      console.log('[getDashboard]', data)
     }
   },
 
@@ -51,6 +84,7 @@ export default {
 
   mounted() {
     console.log('[dashboard mounted]', this.isAuthed)
+    this.getDashboard()
   }
 }
 </script>
